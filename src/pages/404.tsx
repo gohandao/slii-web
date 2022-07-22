d152de882073fb5bba0c32d176cc0da6604cf7a8
iku.takagishi <i.takagishi@gmail.com>
2022-07-22 11:48:46 +0900

    fixed

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -35,7 +35,7 @@ export const Custom404: NextPage = () => {
                 <span className="sr-only">Error</span>404
               </h2>
               <p className="text-2xl font-semibold md:text-3xl">
-                Sorry, we couldn't find this page.
+                Sorry, we could not find this page.
               </p>
               <p className="mt-4 mb-8 text-gray-400">
                 But dont worry, you can find plenty of other things on our

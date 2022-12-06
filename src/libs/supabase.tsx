05fdad2fa68fa66a7921a4b24b3ccbf6e6d57a49
katayama tatsuhumi <t-katayama@cries.co.jp>
2022-12-06 17:47:36 +0900

    JS to TS

--- src/libs/supabase.tsx
+++ src/libs/supabase.tsx
@@ -2,10 +2,12 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABSE_ANON_KEY;
+if (!supabaseUrl) throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
+if (!supabaseAnonKey) throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_KEY");
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-export const getImageUrl = async (path) => {
-  let url;
+export const getImageUrl = async (path: string) => {
+  let url: Blob | null;
   if (path) {
     const storage_name = path.substr(0, path.indexOf("/"));
     const file_path = path.substr(path.indexOf("/") + 1);

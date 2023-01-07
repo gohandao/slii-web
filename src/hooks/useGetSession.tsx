0dd4c06dd1ca7bfdc3e742d5fc13e633674573c7
katayama8000 <tattu.0310@gmail.com>
2023-01-07 13:49:31 +0900

    feat: change useGetSession

--- src/hooks/useGetSession.tsx
+++ src/hooks/useGetSession.tsx
@@ -1,19 +1,19 @@
-import type { User } from "@supabase/supabase-js";
+import type { Session } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 
 import { supabase } from "@/libs/supabase";
 
-export const useGetSessionUser = () => {
-  const [user, setUser] = useState<User>();
+export const useGetSession = () => {
+  const [session, setSession] = useState<Session>();
   useEffect(() => {
     const fetchUser = async () => {
       const {
         data: { session },
       } = await supabase.auth.getSession();
-      if (session) setUser(session.user);
+      if (session) setSession(session);
     };
     fetchUser();
-  }, [user]);
+  }, []);
 
-  return { user };
+  return { session };
 };

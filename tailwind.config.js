87f39236b041a25a797bf8f2d0b2582463e481e8
iku.takagishi <i.takagishi@gmail.com>
2023-02-06 13:07:41 +0900

    add xs screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "480px",
+      ...defaultTheme.screens,
+    },
     extend: {
       fontFamily: {
         outfit: ["Outfit", "sans-serif"],
@@ -13,7 +15,7 @@ module.exports = {
   },
   variants: {
     extend: {
-      backgroundOpacity: ['active'],
+      backgroundOpacity: ["active"],
     },
   },
   plugins: [require("@tailwindcss/line-clamp")],
